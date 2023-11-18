bce6ac4c9058f8d0166378f5fade458921a802a1
Anurag <anuragv8269@gmail.com>
2023-11-18 18:47:24 +0530

    postController created to save posts in db

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,13 @@
-module.exports.post = function(req, res){
-    res.end('<h3>Posts Controller working</h3>');
+const Post = require('../models/post');
+
+module.exports.create = async function(req, res){
+    try{
+        Post.create({
+            content : req.body.content,
+            user : req.user._id
+        })
+        return res.redirect('back');
+    } catch(err){
+        console.log("Error in creating Pots",err);
+    }  
 }
