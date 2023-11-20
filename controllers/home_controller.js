33d218642fd905e5ba5fc1e08c386c9dd2d0672f
Anurag <anuragv8269@gmail.com>
2023-11-20 23:27:30 +0530

    printed comments and its users through pupulate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,14 @@ const Post = require('../models/post')
 module.exports.home = async function (req, res){
     try {
         // populate the user
-        let newPost = await Post.find({}).populate('user')
+        let newPost = await Post.find({})
+        .populate('user')
+        .populate({
+            path : 'comments',
+            populate : {
+                path : 'user'
+            }
+        })
         if(newPost){
             return res.render('home', {
                 title : "Codial HomePage",
